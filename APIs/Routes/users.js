17ca96a8553d6fd3ef6f30b2d10b20ae61072703
benod <benoitnda35@gmail.com>
2023-01-18 10:13:20 +0200

    handling cookies on logout

--- APIs/Routes/users.js
+++ APIs/Routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { createUser, login } from "../controllers/authController/users";
 import { logout } from "../controllers/authController/logout";
+//import { auth } from "../middlewares/auth";
 export const UserRouter = express.Router();
 UserRouter.route("/register").post(createUser);
 UserRouter.route("/login").post(login);
--- APIs/controllers/authController/logout.js
+++ APIs/controllers/authController/logout.js
@@ -4,9 +4,10 @@ import { asyncWrapper } from "../../middlewares/asyncWrapper";
 export const logout = asyncWrapper(async (req, res) => {
     const cookie = req.headers?.cookie;
       if (cookie) {
-      let cookieValue=cookie.split(';');
-       const ActiveRefreshToken= cookieValue[0].substring(13);
-       let user = await User.findOne({ refreshToken:ActiveRefreshToken}).exec();
+      let cookieValues=cookie.split(';');
+      const ActiveRefreshToken=cookieValues.find(value=>value.startsWith('refreshToken')).substring(13);
+      if(!ActiveRefreshToken)return res.json({message:'logged out'})
+      let user = await User.findOne({ refreshToken:ActiveRefreshToken}).exec();
       //delete refresh token in databse
         user.refreshToken=''
         await user.save()
