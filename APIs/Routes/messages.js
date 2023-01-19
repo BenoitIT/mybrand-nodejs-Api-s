379a9f9f79ed6515b88cad7ddad347a74ea7fe11
benod <benoitnda35@gmail.com>
2023-01-19 16:33:23 +0200

    adding Api Documentation

--- APIs/Routes/messages.js
+++ APIs/Routes/messages.js
@@ -8,5 +8,7 @@ import {
 } from "../controllers/messages";
 export const MessageRouter = express.Router();
 //messages routes
-MessageRouter.route("/").get(auth,getAll).post(createMessage);
-MessageRouter.route("/message/:id").get(auth,readOne).delete(auth,deleteMes);
+MessageRouter.get('/Api/messages/all',auth,getAll);
+MessageRouter.post('/Api/messages/new',createMessage);
+MessageRouter.get("messages/message/:id",auth,readOne);
+MessageRouter.delete("messages/message/delete/:id",auth,deleteMes);
--- APIs/server.js
+++ APIs/server.js
@@ -8,42 +8,46 @@ import { ConnectDb } from "./connections/mongDBconnect";
 import {BlogRouter} from "./Routes/blogs";
 import {MessageRouter} from "./Routes/messages";
 import  { UserRouter} from "./Routes/users";
+import { docrouter } from "./documentation/swagger.doc";
 import  {
   handleBadRequest,
   handleNotefound,
   handleInternalServerError,
 } from "./middlewares/handleNotefound";
+
 const app = express();
 const jSend = new JSend({ name: 'mybrand', version: 'X.X.X', release: 'XX' });
 dotenv.config();
+//middlewares
+app.use(docrouter);
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.json());
 app.use(jSend.middleware.bind(jSend))
-
+//routes
 app.use(BlogRouter);
-app.use('/messages',MessageRouter);
-app.use('/admin', UserRouter);
+app.use('/Api/messages',MessageRouter);
+app.use('/Api/admin', UserRouter);
 
 app.use(handleBadRequest);
 app.use(handleNotefound);
 app.use(handleInternalServerError);
 
-
+//start app configuration
 const PORT = process.env.PORT;
+app.listen(
+  PORT,
+  console.log(`application is listening to port ${PORT}....`)
+);
 mongoose.set("strictQuery", true);
-const startApp = async (req,res) => {
+const startApp = async () => {
   try{
   const dBConn = await ConnectDb(process.env.mongoDbURL);
   if (dBConn) {
     console.log("database connected successfully");
-    app.listen(
-      PORT,
-      console.log(`application is listening to port ${PORT}....`)
-    );
   }}catch(ex){
-   res.json({message:'could not connect to database'})
+   console.log('could not connect to database')
   }
 }
 startApp();
