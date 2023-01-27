c4d81573800388a67a11c62651775688871a8173
benoitIT <benoitnda35@gmail.com>
2023-01-27 14:02:49 +0200

    adding messages and additional coverage

--- test/message.js
+++ test/message.js
@@ -1,16 +1,15 @@
 const chai = require("chai");
 const expect = require("chai").expect();
 const chaiHttp = require("chai-http");
-const Blog = require("../src/models/blogs");
 const server = require("../src/server");
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe("testing blogs Api", () => {
+describe("testing messages Api", () => {
   /**
    * testing get all blogs routes
    */
-  it("it should GET all the blogs", (done) => {
+  it("it should GET all the messages", (done) => {
     chai
       .request(server)
       .get("/Api/blogs/all")
@@ -20,11 +19,11 @@ describe("testing blogs Api", () => {
         done();
       });
   });
-  it("it should GET single blog", (done) => {
+  it("it should GET single message", (done) => {
     const param = "63bd0e90a59b4c02537643ae";
     chai
       .request(server)
-      .get("/Api/blogs/"+ param)
+      .get("/Api/messages/"+ param)
       .end((err, res) => {
         res.should.have.status(400);
         res.body.should.be.a("object");
@@ -32,29 +31,11 @@ describe("testing blogs Api", () => {
       });
   });
    
-  it("it should delete the blog with matching id by authorized user", (done) => {
+  it("it should delete the message with matching id by authorized user", (done) => {
     const param = "63bd0e90a59b4c02537643ae";
     chai
       .request(server)
-      .delete("/Api/blogs/blog/" + param)
-      .end((err, res) => {
-        res.should.have.status(400);
-        done();
-      });
-  });
-  /**
-   * testing authorized update single blog routes
-   */
-  it("it should update the blog with matching id by authorized user", (done) => {
-    const param = "63bd0e90a59b4c02537643ae";
-    const blog = {
-      title: "testing blog",
-      category: "tests",
-    };
-    chai
-      .request(server)
-      .patch("/Api/blogs/blog/" + param)
-      .send(blog)
+      .delete("/Api/messages/message/" + param)
       .end((err, res) => {
         res.should.have.status(400);
         done();
